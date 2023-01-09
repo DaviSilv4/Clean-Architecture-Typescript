ec63459aa472369758e8dae25666b5910728b824
David Silva <david.br963@gmail.com>
2023-01-09 12:39:11 -0300

    change aserts to use arrays to ensure order quantity of calls

--- src/data/test/mock-cache.ts
+++ src/data/test/mock-cache.ts
@@ -2,32 +2,40 @@ import { SavePurchases } from "@/domain/user-cases";
 import { CacheStore } from "@/data/protocols/cache";
 
 export class CacheStoreSpy implements CacheStore{
-    deleteCallsCount = 0;
-    insertCallsCount = 0;
+    messages: CacheStoreSpy.Message[];
     deleteKey: string;
     insertKey: string;
     insertValue: SavePurchases.Params[] = [];
 
     public delete(): void {
-        this.deleteCallsCount++;
+        this.messages.push(CacheStoreSpy.Message.delete)
         this.deleteKey;
     }
 
     public insert(key: string, value: any): void {
-        this.insertCallsCount++;
+        this.messages.push(CacheStoreSpy.Message.insert)
         this.insertKey;
         this.insertValue = value;
     }
 
     public simulateDeleteError(): void {
         jest.spyOn(CacheStoreSpy.prototype, 'delete').mockImplementationOnce(() => {
+            this.messages.push(CacheStoreSpy.Message.delete);
             throw new Error();
         })
     }
 
     public simulateInsertError(): void {
         jest.spyOn(CacheStoreSpy.prototype, 'insert').mockImplementationOnce(() => {
+            this.messages.push(CacheStoreSpy.Message.insert);
             throw new Error();
         })
+    }
+}
+
+export namespace CacheStoreSpy{
+    export enum Message{
+        delete,
+        insert
     }
 }
